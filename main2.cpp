a9dc5f860a6594d8e2fc47d8141e6251565fdd74
constantinos030 <constantinos.joh@student.mahidol.edu>
2026-03-06 04:25:14 +0000

    working?

--- main2.cpp
+++ main2.cpp
@@ -1,29 +1,49 @@
 #include<iostream>
+#include<vector>
+#include<cstring>
 using namespace std;
 
 #include "stack.h"
 
+bool check_char(char paren, Stack* s);
+
 int main(int argc, char **argv){
-    Stack s;
-   
-/*
-  Exercise 2
- printf("Checking the parentheses in argv arguments\n");
+vector<bool> check(argc-1);
+cout << "Checking the parentheses in argv arguments\n";
 
-   */
+for(int i = 1; i < argc; i++)
+    {
+     Stack* s = new Stack;
+     bool this_str = false;
+     for(int j=0;j<strlen(argv[i]);j++)
+     {
+         bool char_flag = check_char(argv[i][j], s);
 
-    /*
-     for(j=0;j<strlen(argv[i]);j++){
-       // Use stack to help with the parentheses
+         if (argv[i][j]=='{' || argv[i][j] == '[') s->push(argv[i][j]);
 
+         else if (!char_flag) {this_str=false; break;}
+         else if ( char_flag) {if (!s->get_size()){this_str=true;}}
+         else {cout<<"No Condition met\n"; break;}
+     }
+     if (this_str) check[i-1] = true; 
+     else if (!this_str) check[i-1] = false;
+     delete s;
+    }
 
-
-
-
-  }
-
-  */
-
-
+    for (int i = 0; i < argc-1 ; i++)
+    {if (check[i]) cout << "argv " << i+1 << " correct\n";
+     else cout << "argv " << i+1 << " incorrect\n";}
+    
    return 0;
+}
+
+bool check_char(char paren, Stack* s)
+{
+  if (!s->get_size()) return false;
+  switch(paren)
+         {
+          case '}' : if (s->pop()=='{'){return true;} else return false; break;
+          case ']' : if (s->pop()=='['){return true;} else return false; break;
+          default: return true;
+         }
 }
--- stack.h
+++ stack.h
@@ -6,33 +6,35 @@ class Stack {
 	NodePtr top;
 	int size;
 public:
-    void push(int);
-    int pop();
+    void push(char);
+    char pop();
+    int get_size(){return size;};
     Stack(NodePtr t = NULL, int s = 0);
     ~Stack();
 };
 
 
-void Stack::push(int x){
+void Stack::push(char x){
   NodePtr new_node=new NODE(x);
   if(new_node){new_node->set_next(top);
                 top = new_node;  
                 size ++;  
-                cout << "Pushed" << x << endl;
+                cout << "Pushed " << x << endl;
               } 
   else{top = new_node; new_node->set_next(NULL); size++;}
 }
 
-int Stack::pop(){
+char Stack::pop(){
     if (top==NULL){cout<<"Empty Stack"<<endl; return 0;}
     else{
         
         NodePtr t=top;
-        int value;
+        char value;
 
         value=t->get_value();
         top = t->get_next();
 
+        cout << "value = " << value;
         delete t;
         size--;
         return value;
